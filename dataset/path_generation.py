ecc62572a282692deee541ef0535ce9ddd4b3be7
Ubuntu <ubuntu@ip-172-31-29-17.ec2.internal>
2018-10-20 04:51:10 +0000

    changed zip function to np.random.choice

--- dataset/path_generation.py
+++ dataset/path_generation.py
@@ -4,16 +4,16 @@ from openslide import open_slide, ImageSlide
 import scipy.io as sio
 import pdb
 import numpy as np
-
-slide_path = '/home/ubuntu/data/TCGA_LUSC'
+import gc
+slide_path = '/home/ubuntu/Semi-Supervised-GANs/dataset/TCGA_LUSC'
 slides_list = os.listdir(slide_path)
 mask_path = '/home/ubuntu/data/tissue-masks-CMU/'
-tar_path = '/home/ubuntu/data/patches'
+tar_path = '/home/ubuntu/patches'
 #randomly sample 100 slides
 
 print("Randomly Sampling 100 slides")
-#samples = random.sample(slides_list,100)
-samples = slides_list[:50]
+samples = random.sample(slides_list,100)
+#samples = slides_list[:50]
 
 for slide in samples :
 	slide_ID = slide.split('_')[0]
@@ -26,23 +26,29 @@ for slide in samples :
 	image = open_slide(slide_src)
 	csv_path = os.path.join(slide_dest,slide_ID+".csv")
 	g = open(csv_path,'w')
+	#pdb.set_trace()
 	mask_src = os.path.join(mask_path,slide_ID+".mat")
 	mask = sio.loadmat(mask_src)
 	
-	x_coords,y_coords = np.nonzero(mask['mask'])	
+	x_coords,y_coords = np.nonzero(mask['mask'])
+	del mask
+	
+	gc.collect()	
 	x_coords = x_coords*4
 	y_coords = y_coords*4
-	nonzero_coords = list(zip(x_coords,y_coords))
+	#nonzero_coords = list(zip(x_coords,y_coords))
 	#pdb.set_trace()
 	print("Randomly sampling patches ...")
-	sample_coords = random.sample(nonzero_coords,100)
-	for i in range(len(sample_coords)):
-		patch = image.read_region((sample_coords[i][0],sample_coords[i][1]),0,(256,256))
+	sample_idxs = np.random.choice(np.arange(len(x_coords)),100)
+	count = 0	
+	for i in sample_idxs:
+		patch = image.read_region((x_coords[i],y_coords[i]),0,(256,256))
 		patch = patch.convert("RGB")
-		outfile = os.path.join(slide_dest,"patch_"+str(i))
+		outfile = os.path.join(slide_dest,"patch_"+str(i)+".jpg")
 		patch.save(outfile,'JPEG')
-		g.write(("patch_"+str(i)+str(sample_coords[i][0])+","+str(sample_coords[i][1])+"\n"))
-	
+		g.write(("patch_"+str(count)+","+str(x_coords[i])+","+str(y_coords[i])+"\n"))
+		count+=1
+	image.close()	
 	g.close()
 	print("----------------------")
 		
