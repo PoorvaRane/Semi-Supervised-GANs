373faec3e92e861d26728bdc8cb782fdf135e5f4
PoorvaRane <poorvarane99@gmail.com>
2018-11-15 09:50:06 +0000

    128x128 Image generation added

--- src/SGAN_128.py
+++ src/SGAN_128.py
@@ -33,7 +33,7 @@ num_classes = 10
 latent_size = 100
 labeled_rate = 0.1
 num_epochs = 1000
-image_size = 64
+image_size = 128
 batch_size = 32
 epsilon = 1e-8 # used to avoid NAN loss
 generator_frequency = 1
@@ -45,8 +45,8 @@ lr = 1e-5
 b1 = 0.5 # adam: decay of first order momentum of gradient
 b2 = 0.999 # adam: decay of first order momentum of gradient
 
-model_path ='./TCGA_64.tar'
-image_dir = 'tcga_images_64'
+model_path ='./TCGA_128.tar'
+image_dir = 'tcga_images_128'
 
 os.makedirs(image_dir, exist_ok=True)
 
@@ -227,14 +227,14 @@ class DiscriminatorNet(torch.nn.Module):
             nn.LeakyReLU(0.2),
             weight_norm(nn.Conv2d(in_channels=filter1, out_channels=filter1, kernel_size=3, stride=1, padding=1), name='weight'),
             nn.LeakyReLU(0.2),
-            weight_norm(nn.Conv2d(in_channels=filter1, out_channels=filter2, kernel_size=3, stride=2, padding=1), name='weight'),
+            weight_norm(nn.Conv2d(in_channels=filter1, out_channels=filter1, kernel_size=3, stride=2, padding=1), name='weight'),
             nn.LeakyReLU(0.2),
             nn.Dropout2d(dropout_rate)
         )
         
         # CNNBlock 3
         self.wn_conv3 = nn.Sequential(
-            weight_norm(nn.Conv2d(in_channels=filter2, out_channels=filter2, kernel_size=3, stride=1, padding=1), name='weight'),
+            weight_norm(nn.Conv2d(in_channels=filter1, out_channels=filter2, kernel_size=3, stride=1, padding=1), name='weight'),
             nn.LeakyReLU(0.2),
             weight_norm(nn.Conv2d(in_channels=filter2, out_channels=filter2, kernel_size=3, stride=1, padding=1), name='weight'),
             nn.LeakyReLU(0.2),
@@ -245,6 +245,17 @@ class DiscriminatorNet(torch.nn.Module):
         
         # CNNBlock 4
         self.wn_conv4 = nn.Sequential(
+            weight_norm(nn.Conv2d(in_channels=filter2, out_channels=filter2, kernel_size=3, stride=1, padding=1), name='weight'),
+            nn.LeakyReLU(0.2),
+            weight_norm(nn.Conv2d(in_channels=filter2, out_channels=filter2, kernel_size=3, stride=1, padding=1), name='weight'),
+            nn.LeakyReLU(0.2),
+            weight_norm(nn.Conv2d(in_channels=filter2, out_channels=filter2, kernel_size=3, stride=2, padding=1), name='weight'),
+            nn.LeakyReLU(0.2),
+            nn.Dropout2d(dropout_rate)
+        )
+        
+        # CNNBlock 5
+        self.wn_conv5 = nn.Sequential(
             weight_norm(nn.Conv2d(in_channels=filter2, out_channels=filter2, kernel_size=3, stride=1, padding=0), name='weight'),
             nn.LeakyReLU(0.2),
             weight_norm(nn.Conv2d(in_channels=filter2, out_channels=filter2, kernel_size=1, stride=1, padding=0), name='weight'),
@@ -265,6 +276,7 @@ class DiscriminatorNet(torch.nn.Module):
         x = self.wn_conv2(x)
         x = self.wn_conv3(x)
         x = self.wn_conv4(x)
+        x = self.wn_conv5(x)
         
         # Linear
         flatten = x.mean(dim=3).mean(dim=2)
@@ -301,8 +313,13 @@ class GeneratorNet(torch.nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU()
         )
-        self.wn_deconv4 = nn.Sequential(
-            weight_norm(nn.ConvTranspose2d(in_channels=64, out_channels=3, kernel_size=5, stride=2, padding=2, output_padding=1),
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(in_channels=64, out_channels=32, kernel_size=5, stride=2, padding=2, output_padding=1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU()
+        )
+        self.wn_deconv5 = nn.Sequential(
+            weight_norm(nn.ConvTranspose2d(in_channels=32, out_channels=3, kernel_size=5, stride=2, padding=2, output_padding=1),
                         name='weight'),
             nn.Tanh()
         )
@@ -317,7 +334,8 @@ class GeneratorNet(torch.nn.Module):
         x = self.deconv1(x)
         x = self.deconv2(x)
         x = self.deconv3(x)
-        x = self.wn_deconv4(x)
+        x = self.deconv4(x)
+        x = self.wn_deconv5(x)
 
         return x
 
@@ -466,7 +484,7 @@ def train_generator(optimizer_G, b_size, epsilon):
 # In[ ]:
 
 def save_checkpoint(state, model_type):
-    torch.save(state, model_type + '_checkpoint_64.tar')
+    torch.save(state, model_type + '_checkpoint_128.tar')
 
         
 # In[ ]:
